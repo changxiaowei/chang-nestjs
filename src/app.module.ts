dc0282dab82a967a027d0f6c78c38dfe629c4c8f
changxw <1035988994@qq.com>
2020-04-11 10:32:37 +0800

    logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerService } from './logger.service';
+
+const mockAppService = {
+    getHello(){
+        return 'test'
+    }
+}
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [{
+      provide: AppService,
+      useClass: AppService
+  }, {
+      provide: "LOGGER",
+      useClass: LoggerService
+  },
+{
+    provide: "DEMO-Factory",
+    useFactory: () => {
+        return 'factory'
+    }
+}],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, Inject } from '@nestjs/common';
+import { LoggerService } from './logger.service'
 @Injectable()
 export class AppService {
+    constructor( @Inject('LOGGER')private readonly logger: LoggerService, @Inject('DEMO-Factory') demo: string){
+        console.log(demo);
+    }
   getHello(): string {
+      this.logger.log('Appservie: Hello World');
     return 'Hello World!';
   }
 }
